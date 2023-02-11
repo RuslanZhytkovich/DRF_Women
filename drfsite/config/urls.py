73739e53fe50ebbf8016f5a705b02451d8e8cfef
ruslan <rzhitkovich@bk.ru>
2023-02-11 19:23:33 +0300

    Add UpdateAPIUpdate Add WomenAPIDetailView to make Delete, Edit

--- drfsite/config/urls.py
+++ drfsite/config/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/womenlist/', WomenAPIList.as_view()),
     path('api/v1/womenlist/<int:pk>/', WomenAPIList.as_view()),
+    path('api/v1/womendetail/<int:pk>/', WomenAPIDetailView.as_view()),
 ]
